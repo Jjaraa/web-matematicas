f1c0175ad0d584dd06191e43812400f6dc04699f
Jonathan <jjara21@ufromail.cl>
2021-08-18 19:23:44 -0400

    se agregan funciones para calcular figuras geometricas

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # web-matematicas
 Web de matematicas del curso practico de js de platzi
+
+## Taller #1: Figuras geométricas 
+
---
+++ figuras.js
@@ -0,0 +1,25 @@
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+function perimetroTriangulo(ladoa, ladob, ladoc) {
+  return ladoa + ladob + ladoc;
+}
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo(radio) {
+    return 2 * PI * radio;
+}
+
+function areaCirculo(radio) {
+    return PI * radio * radio;
+}
